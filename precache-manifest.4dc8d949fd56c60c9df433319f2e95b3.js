07d1be4204dcda7dd75ce07496086cf59c20d835
Alc499 <Ali.vitchenko@gmail.com>
2019-08-15 19:17:10 +0300

    Updates

---
+++ precache-manifest.4dc8d949fd56c60c9df433319f2e95b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a808bf1da4fadd54b1df70b1d25eba",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "4dc1e1b28cccd4040731",
+    "url": "/react_redux-list-of-todos/static/css/main.75d4a33d.chunk.css"
+  },
+  {
+    "revision": "0520757d5fb9aff59871",
+    "url": "/react_redux-list-of-todos/static/js/2.a2aea1b4.chunk.js"
+  },
+  {
+    "revision": "4dc1e1b28cccd4040731",
+    "url": "/react_redux-list-of-todos/static/js/main.b9861164.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
